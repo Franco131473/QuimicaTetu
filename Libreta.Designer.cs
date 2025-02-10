12ae7408e0e086cfc7e1f2c6e0de2e2775fc95cc
Franco131473 <154501391+Franco131473@users.noreply.github.com>
2025-02-10 05:39:08 -0300

    Add files via upload

---
+++ Libreta.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace QuimicaTetu
+{
+    partial class _Libreta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(157, 100);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(947, 543);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Karina",
+            "Dominga",
+            "Rubén"});
+            this.comboBox1.Location = new System.Drawing.Point(20, 35);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(268, 37);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Agregar a la Cuenta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 109);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Eliminar Deuda";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Karina",
+            "Dominga",
+            "Rubén"});
+            this.comboBox2.Location = new System.Drawing.Point(633, 36);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(268, 37);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1222, 564);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1222, 627);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(513, 17);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 78);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Agregar nuevo Cliente";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(31, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 89);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(1141, 17);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 119);
+            this.panel2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese Nombre:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(55, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 33);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Agregar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(1141, 159);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 174);
+            this.panel3.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(24, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 26);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 135);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 34);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.comboBox3);
+            this.panel4.Controls.Add(this.textBox4);
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Location = new System.Drawing.Point(4, 173);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(146, 185);
+            this.panel4.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(17, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 0;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Karina",
+            "Dominga",
+            "Rubén"});
+            this.comboBox3.Location = new System.Drawing.Point(17, 27);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(117, 33);
+            this.comboBox3.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 479);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // _Libreta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "_Libreta";
+            this.Text = "_Libreta";
+            this.Load += new System.EventHandler(this._Libreta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Libreta.cs
@@ -0,0 +1,271 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuimicaTetu
+{
+    public partial class _Libreta : Form
+    {
+        public bool banderaL = false;
+        private AccesoDatos datosbase;
+        private VentaManual vm;
+        public _Libreta(AccesoDatos datosbase)
+        {
+            vm = new VentaManual();
+            InitializeComponent();
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel4.Visible = false;
+            this.datosbase = datosbase;
+            if (datosbase.Nombre.Count > 0)
+            {
+                panel1.Visible = true;
+                //button2.Visible = true;
+            }
+            else
+            {
+                comboBox1.Visible = false;
+                //button2.Visible = false;
+            }
+            CargarYMostrarProductos();
+            SumarTotal();
+           // Visivilidad();
+        }
+
+        private void Visivilidad()
+        {
+            if (banderaL == true)
+            {
+                comboBox1.Visible = true;
+            }
+            else
+            {
+                comboBox1.Visible = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Focus();
+        }
+        private void CargarYMostrarProductos()
+        {
+            //productos = new Productos();
+            datosbase.CargarDatosLibreta(datosbase.ruta5);
+            // Ordenar la lista antes de asignarla al DataGridView  
+            //var listaOrdenada = datobase.listaP.OrderBy(item => item.Cantidad).ToList(); // Asegúrate que Cantidad es accesible  
+            dataGridView1.DataSource = datosbase.listaLibreta;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //datosbase = new AccesoDatos();
+            MessageBox.Show(datosbase.Nombre.Count.ToString());
+            datosbase.GuardarListaLibreta(comboBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EliminarElementos(comboBox1.Text, datosbase.ruta5);
+        }
+
+        public void EliminarElementos(string nombreElemento, string rutaArchivoJson)
+        {
+            // Leer el archivo JSON
+            string json = File.ReadAllText(rutaArchivoJson);
+            List<ArchivoLibreta> items = JsonConvert.DeserializeObject<List<ArchivoLibreta>>(json);
+
+            // Filtrar los elementos que no cumplen con la condición
+            List<ArchivoLibreta> itemsFiltrados = items.FindAll(item => item.Cliente != nombreElemento);
+
+            // Guardar los cambios en el archivo JSON
+            string nuevoJson = JsonConvert.SerializeObject(itemsFiltrados, Formatting.Indented);
+            File.WriteAllText(rutaArchivoJson, nuevoJson);
+        }
+
+        private void _Libreta_Load(object sender, EventArgs e)
+        {
+           // Visivilidad();
+        }
+        bool activo = false;
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            activo = true;
+            string nombreRecibido = comboBox2.Text;
+            var listaArchivo4 = datosbase.CargarArchivo4(datosbase.ruta5);
+            List<ResultadoFiltrado> resultadoFiltrados;
+            var resultadoNombre = listaArchivo4
+                .Where(a => a.Cliente.Equals(nombreRecibido, StringComparison.OrdinalIgnoreCase))
+                        .Select(a => new ResultadoFiltrado
+                        {
+                            Cliente = a.Cliente,
+                            NombreProducto = a.NombreProducto,
+                            Tipo = a.Tipo,
+                            Cantidad = a.Cantidad,
+                            PrecioVenta = a.PrecioVenta,
+                            PrecioCompra = a.PrecioCompra,
+                            Ganancia = a.Ganancia,
+                            Fecha = a.Fecha,
+                            Hora = a.Hora
+                        })
+                        .ToList();
+            resultadoFiltrados = resultadoNombre;
+            dataGridView1.DataSource = resultadoFiltrados;
+            SumarTotal();
+            textBox3.Enabled = true;
+            textBox3.ReadOnly = true;
+        }
+
+        public class ResultadoFiltrado
+        {
+            public string Cliente { get; set; }
+            public string NombreProducto { get; set; }
+            public string Tipo { get; set; }
+            public int Cantidad { get; set; }
+            public int PrecioVenta { get; set; }
+            public int PrecioCompra { get; set; }
+            public int Ganancia { get; set; }
+            public string Fecha { get; set; }
+            public string Hora { get; set; }
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.ColumnIndex == 4 && e.Value != null)
+            {
+                if (int.TryParse(e.Value.ToString(), out int value))
+                {
+                    // Formatear el valor como "1.000,00"
+                    e.Value = value.ToString("C", new CultureInfo("es-AR")); e.FormattingApplied = true;
+                }
+            }
+            if (e.ColumnIndex == 5 && e.Value != null)
+            {
+                if (int.TryParse(e.Value.ToString(), out int value))
+                {
+                    // Formatear el valor como "1.000,00"
+                    e.Value = value.ToString("C", new CultureInfo("es-AR")); e.FormattingApplied = true;
+                }
+            }
+            if (e.ColumnIndex == 6 && e.Value != null)
+            {
+                if (int.TryParse(e.Value.ToString(), out int value))
+                {
+                    // Formatear el valor como "1.000,00"
+                    e.Value = value.ToString("C", new CultureInfo("es-AR")); e.FormattingApplied = true;
+                }
+            }
+        }
+        private void SumarTotal()
+        {
+            int acumuladorTotal = 0;
+            int acumuladorGanancia = 0;
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                if (!fila.IsNewRow)
+                {
+                    // Extraer y limpiar el valor de la celda (8 y 9)  
+                    string precioTexto = fila.Cells[4].Value?.ToString();
+                    string gananciaTexto = fila.Cells[6].Value?.ToString();
+
+                    // Limpiar el string para convertirlo a un número  
+                    if (!string.IsNullOrEmpty(precioTexto))
+                    {
+                        precioTexto = precioTexto.Replace(".", "").Replace(",", "");
+                        if (int.TryParse(precioTexto, out int precio))
+                        {
+                            acumuladorTotal += precio;
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(gananciaTexto))
+                    {
+                        gananciaTexto = gananciaTexto.Replace(".", "").Replace(",", "");
+                        if (int.TryParse(gananciaTexto, out int ganancia))
+                        {
+                            acumuladorGanancia += ganancia;
+                        }
+                    }
+                }
+            }
+            if (!activo)
+            {
+                label1.Text = acumuladorTotal.ToString("C");
+                label2.Text = acumuladorGanancia.ToString("C");
+            }
+            else
+            {
+                textBox3.Text = acumuladorTotal.ToString("C");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+            textBox1.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string newItem = textBox1.Text;
+
+            // Verifica que el TextBox no esté vacío
+            if (!string.IsNullOrWhiteSpace(newItem))
+            {
+                // Agrega el nuevo ítem al ComboBox
+                comboBox1.Items.Add(newItem);
+                comboBox2.Items.Add(newItem);
+                // Limpia el TextBox
+                textBox1.Clear();
+                panel2.Visible = false;
+                MessageBox.Show("Agregado Correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un ítem válido.");
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var listaLib = new ArchivoLibreta
+                {
+                    Cliente = comboBox3.Text,
+                    NombreProducto = "Efectivo",
+                    Tipo = "Prestamo",
+                    Cantidad = int.Parse(textBox4.Text),
+                    PrecioVenta = int.Parse(textBox4.Text),
+                    PrecioCompra = int.Parse(textBox4.Text),
+                    Ganancia = 0,
+                    Fecha = label5.Text,
+                    Hora = label4.Text
+                };
+                datosbase.listaLibreta.Add(listaLib);
+                datosbase.GuardarListaLibreta(datosbase.ruta5);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el archivo: {ex.Message}");
+            }
+            textBox4.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Text = DateTime.Now.ToShortTimeString();
+            label5.Text = DateTime.Now.ToShortDateString();
+        }
+    }
+}
